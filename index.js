83905ac35edc29d31354a47de476cf525f531fa7
Greta Ra <ggretute88@gmail.com>
2025-09-03 16:24:33 +0200

    Add logic to generate passwords

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
+
+let output1El = document.getElementById("output-1")
+let output2El = document.getElementById("output-2")
+
+function getRandomNumber(){
+  return Math.floor(Math.random() * characters.length) 
+}
+
+function getPasswords (){
+  let password1 = ""
+  let password2 = ""
+
+  for (let i = 0; i < 15; i++){
+    password1 += characters[getRandomNumber()]
+    password2 += characters[getRandomNumber()]
+    
+  }
+  output1El.textContent = password1
+  output2El.textContent = password2
+}
